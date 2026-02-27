0b74f4b002024fa705d7a672968d5c3fc0a792cf
Alasco Prescott <alascoprescott1997@gmail.com>
2026-02-27 06:48:14 -0500

    moved og root page to Dashboard, started landing page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,14 +9,8 @@ export default function RootLayout({
 }) {
   return (
     <html>
-      <body className='min-h-screen flex flex-row p-6'>
-        <nav className='bg-gray-100 grow p-2 rounded-lg'>
-          <SideBar />  
-        </nav>
-        <main className='grow-2 p-2'>
-          <Heading />
+      <body>
           {children}
-        </main>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
+import React from 'react';
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 
 
+//TODO: finish the rest of landing page, see landing page design.
 export default function Page() {
   return (
     <>
-    
+    <DotLottieReact
+      src="/Robot-Bot 3D.json"
+      loop
+      autoplay
+    />
     </>
-  )
-}
+  );
+};
